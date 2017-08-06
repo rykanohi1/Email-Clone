3097578ab5dc16a5128458bff9a0677f39f20293
rykanohi1 <rykanohi1@hotmail.com>
2017-08-06 12:44:07 +0200

    completed folder-count component

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,14 +5,19 @@
 // IMPORTANT
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
+const path = require('path');
 
 module.exports = {
   plugins: [
     // your custom plugins
   ],
   module: {
-    loaders: [
-      // add your custom loaders.
+    rules: [
+      {
+        test: /\.*css$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"],
+        include: path.resolve(__dirname, '../')
+      }
     ],
   },
 };

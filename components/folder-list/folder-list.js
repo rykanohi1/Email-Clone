56818c88d082b328379a8d9929bb3408222253a3
Joe Crick <joe@josephcrick.com>
2017-09-06 03:37:43 +0100

    First passing test for folder-list

--- components/folder-list/folder-list.js
+++ components/folder-list/folder-list.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import styles from './folder-count.scss';
+import Folder from '../folder/folder';
 
-export default props => {
-    <span> 
-    </span>
-} 
+export default ({folders}) => {
+    return (<div className="folder-list">
+        {folders.map(folder => <Folder
+            key={folder.id} 
+            label={folder.label} 
+            count={folder.count} 
+            has-unread={folder.hasUnread}/>
+        )}
+    </div>)
+}

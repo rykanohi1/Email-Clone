cc8135a1004683e7c39dc1481dcab47514590e25
Joe Crick <joe@josephcrick.com>
2017-09-13 04:27:19 +0100

    Selected folder test passes

--- components/folder/folder.js
+++ components/folder/folder.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import FolderCount from './folder-count/folder-count'
 
-export default ({label, count, hasUnread}) => (
-    <div className="folder">
+export default ({label, count, hasUnread, isSelected}) => (
+    <div className={`folder ${isSelected ? 'selected' : ''}`}>
         <span className="folder-title"> {label} </span>
         <FolderCount hasUnread={hasUnread} count={count}/>
     </div>

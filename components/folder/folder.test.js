2221f5deceb02957e4d906a278d9c4826ff2f2f9
Joe Crick <joe@josephcrick.com>
2017-09-13 04:24:16 +0100

    Style folder list

--- components/folder/folder.test.js
+++ components/folder/folder.test.js
@@ -9,12 +9,12 @@ describe ('Folder', ()=> {
         const actual = app.find('.folder-title').text().trim();
 
         expect(actual).toEqual(expected);
-    })
+    });
     test ('should have selected class when folder is selected', () => {
         const expected = 1;
         const app = shallow(<Folder isSelected={true}/>);
         const actual = app.find('.selected').length;
         
         expect(actual).toEqual(expected);
-    })
+    });
 });

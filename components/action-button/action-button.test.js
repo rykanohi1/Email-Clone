a6293c59ee734c082c8734e14854a9e0d844f4a2
Joe Crick <joe@josephcrick.com>
2017-09-20 03:36:17 +0100

    incremental

---
+++ components/action-button/action-button.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import ActionButton from './action-button';
+
+describe ('ActionButton', () => {
+  test('should display an icon when provided with an image', () => {
+    const image = '';
+  })
+});

45d488b82c56ed8bde12e63280116373ec9ea85c
Joe Crick <joe@josephcrick.com>
2017-10-06 03:26:52 +0100

    added test for label/imgSrc contract

--- components/action-button/action-button.js
+++ components/action-button/action-button.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 // What do I expect from my callers
-export default ({imgSrc, label, title}) => {
+/**
+ * Renders an action button
+ * @param {String} imgSrc
+ * @param {String} label
+ * @param {String} title
+ * @param {Function} action
+ * @returns {XML}
+ */
+export default ({imgSrc, label, title, action}) => {
   if(!(imgSrc || label)) {
     throw new Error('imgSrc and label cannot both be blank');
+  }
+  if(!action) {
+    throw new Error('must provide an action to the button');
   }
   return (
     // In the case of most attributes, If the attribute is falsy, React will not render it

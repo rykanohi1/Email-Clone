44dd914dcec68be99427fac8cdc6a03d4b316144
rykanohi1 <rykanohi1@hotmail.com>
2017-10-23 16:02:59 -0700

    passed test for drop-down list

--- components/drop-down/drop-down.js
+++ components/drop-down/drop-down.js
@@ -1,2 +1,13 @@
+import React from 'react';
 
-
+export default ({items}) => {
+    return (
+    <div>
+        <ul>
+            {items.map(function(itemValue){
+                return <li>{itemValue}</li>
+            })}
+        </ul>
+    </div>
+    );
+}

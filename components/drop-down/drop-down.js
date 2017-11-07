fe9c0583a94e44a92fbb73c8d3182b7a1ba0342d
Joe Crick <joe@josephcrick.com>
2017-11-07 04:16:44 +0100

    Stuff

--- components/drop-down/drop-down.js
+++ components/drop-down/drop-down.js
@@ -1,16 +1,22 @@
-// @flow
 import React from 'react';
 
 export default ({items}) => {
-    const itemType = items.map(item => typeof item);
-    if(itemType !== 'object'){
-        throw new Error('must provide valid items');
+
+  if(!Array.isArray(items)) {
+    throw new Error('must be an array')
+  }
+
+  items.forEach(item => {
+    if(typeof item !== 'object' && !('id' in item) && !('value' in item)) {
+      throw new Error('invalid object')
     }
+  });
+
   return (
     <div>
-        <ul>
-            {items.map(item => <li key={item.id}>{item.value}</li>)}
-        </ul>
+      <ul>
+        {items.map(item => <li key={item.id}>{item.value}</li>)}
+      </ul>
     </div>
-    );
+  );
 }

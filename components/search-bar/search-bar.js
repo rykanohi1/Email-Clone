f13fc80eda68855536e31c903b87087bd2af26fa
BuildTools <unconfigured@null.spigotmc.org>
2017-12-21 12:26:40 -0800

    added search-bar component

---
+++ components/search-bar/search-bar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ActionButton from '../action-button/action-button';
+
+export default () => {
+    search = () => {
+        
+    };
+    
+    <div>
+        <input type="text"/>
+        <ActionButton title="search" imgSrc={imgSrc} action={search()}/>
+    </div>
+}

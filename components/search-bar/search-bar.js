a332800ef30b0317c3c69457f25355766d571ab8
BuildTools <unconfigured@null.spigotmc.org>
2017-12-28 16:32:42 -0800

    created new test for search-bar

--- components/search-bar/search-bar.js
+++ components/search-bar/search-bar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ActionButton from '../action-button/action-button';
 
-export default () => {
-    search = () => {
-        
-    };
-    
+export default ({imgSrc}); 
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            hint: 'search mail'
+        }
+    }
+   render() { 
+    return (
     <div>
-        <input type="text"/>
-        <ActionButton title="search" imgSrc={imgSrc} action={search()}/>
+        <input type="text" defaultValue={this.state.hint}/>
+        <ActionButton title="search" imgSrc={this.props.imgSrc} action={search()}/>
     </div>
+    );
+   };
 }
--- components/search-bar/search-bar.test.js
+++ components/search-bar/search-bar.test.js
@@ -3,5 +3,10 @@ import shallow from 'enzyme';
 import SearchBar from './search-bar';
 
 describe ('SearchBar', () => {
-        test('sh')
+        test('should display a default search value in the text bar if given one', () => {
+                const expected = true;
+                const app = shallow (<SearchBar imgSrc="blank"/>);
+                const actual = app.find('input').defaultValue
+                expect(actual).toEqual(expected);
+        })
 });

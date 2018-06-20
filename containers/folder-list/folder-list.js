fb4281db54bff98fb3b1af765a707f9ba06718c9
Robert Yaron <rykanohi1@hotmail.com>
2018-06-20 13:11:35 -0700

    added first action and reducer to folder-list container

--- containers/folder-list/folder-list.js
+++ containers/folder-list/folder-list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './folder-list.scss';
-import Folder from '../folder/folder';
+import Folder from './folder/folder';
 
 export default ({folders}) => {
     return (<div className="folder-list">
